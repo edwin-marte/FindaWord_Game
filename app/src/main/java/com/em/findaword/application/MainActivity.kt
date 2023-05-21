d9632dbb11fe0d3cfbf441c702e9662f88ffcbbf
Edwin <edwin25mt@gmail.com>
2023-05-21 09:24:57 -0400

    create main activity

---
+++ app/src/main/java/com/em/findaword/application/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.em.findaword.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.em.findaword.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
